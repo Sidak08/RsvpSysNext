ce49b8d304218cbe9cded73614bc58d44d4ef2b0
Sidak <magicalsidak@gmail.com>
2024-07-08 15:36:35 -0400

    workign on the nav fin the desktop view

--- rsvp-sys/src/app/components/navbar.jsx
+++ rsvp-sys/src/app/components/navbar.jsx
@@ -1,24 +1,38 @@
 import Link from "next/link";
 import Image from "next/image";
-import LogoSvg from "./svg.jsx";
 
 const Navbar = () => {
   return (
-    <div className="flex flex-row justify-between items-center">
-      <div className="flex flex-row justify-between items-center">
-        {/* logo and titel */}
-        <LogoSvg />
+    <div className="flex flex-row justify-between items-center w-full mt-7">
+      <div className="flex flex-row justify-between items-center ml-8 ">
+        <div className="w-[74px] h-[74.36px] relative">
+          <div className="w-[17.18px] h-[17.18px] left-0 top-[54.26px] absolute origin-top-left rotate-[-1.23deg] bg-indigo-900 rounded-full" />
+          <div className="w-[17.18px] h-[17.18px] left-[56.46px] top-[54.45px] absolute origin-top-left rotate-[-1.23deg] bg-purple-700 rounded-full" />
+          <div className="w-[52.16px] h-[18.14px] left-[7.73px] top-[44.71px] absolute origin-top-left rotate-[-59deg] bg-purple-700 rounded-[23px]" />
+          <div className="w-[46.40px] h-[18.14px] left-[24.47px] top-[65.02px] absolute origin-top-left rotate-[-59deg] bg-indigo-900 rounded-[23px]" />
+        </div>
+        <div className="w-[200px] h-[38px] text-purple-700 text-5xl font-normal font-['Inika'] ml-8">
+          QuikSeat
+        </div>
       </div>
-      <div>
+      <div className="w-[350px] flex justify-between items-center mr-4">
         <Link href="/chat">
-          <a>chat</a>
+          <div className="text-white text-[22px] font-normal font-['Inika']">
+            Chat
+          </div>
         </Link>
         <Link href="/dashboard">
-          <a>dashboard</a>
+          <div className="text-white text-[22px] font-normal font-['Inika']">
+            Dashboard
+          </div>
         </Link>
-        <Link href="/pricing">
-          <a>pricing</a>
-        </Link>
+        <div className="w-[130px] h-[50px] bg-indigo-700 rounded-[10px] flex justify-center items-center">
+          <Link href="/pricing">
+            <div className="text-white text-2xl font-normal font-['Inika']">
+              Pricing
+            </div>
+          </Link>
+        </div>
       </div>
     </div>
   );
