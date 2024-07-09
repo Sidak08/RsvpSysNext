0ba1f021fcfd1c5dc7928b47d22c643f46e22b8c
Sidak <magicalsidak@gmail.com>
2024-07-08 22:37:31 -0400

    man i wana cry

--- rsvp-sys/src/app/components/navbar.jsx
+++ rsvp-sys/src/app/components/navbar.jsx
@@ -28,9 +28,9 @@ const Navbar = () => {
           <div className="w-[29.47px] h-[11.52px] left-[15.54px] top-[41.30px] absolute origin-top-left rotate-[-59deg] bg-indigo-900 rounded-[23px]" />
         </div>
         {/* Render this element on small screens */}
-        <div className="block md:hidden w-[161px] h-[38px] text-purple-700 text-[38px] font-normal font-['Inika']  ml-[11px] mb-3">
+        {/* <div className="block md:hidden w-[161px] h-[38px] text-purple-700 text-[38px] font-normal font-['Inika']  ml-[11px] mb-3">
           QuikSeat
-        </div>
+        </div> */}
 
         {/* Render this element on large screens */}
         <div className="hidden md:block w-[200px] h-[38px] text-purple-700 text-5xl font-normal font-['Inika'] ml-[38px] ">
@@ -38,7 +38,7 @@ const Navbar = () => {
         </div>
       </div>
 
-      <div className="w-[350px] flex justify-between items-center mr-4 hidden md:flex">
+      {/* <div className="w-[350px] flex justify-between items-center mr-4 hidden md:flex">
         <Link href="/chat">
           <div className="text-white text-[22px] font-normal font-['Inika']">
             Chat
@@ -56,17 +56,17 @@ const Navbar = () => {
             </div>
           </Link>
         </div>
-      </div>
+      </div> */}
 
-      <div className="absolute block md:hidden mr-4 w-full">
+      <div className="absolute top-0 right-0 flex justify-end md:hidden mr-4">
         <button
           onClick={onClick}
           className={`${renderMobileMenu ? "hidden" : "fade-in"}`}
         >
-          <div className="w-[30px] h-[27px] relative">
-            <div className="w-[30px] h-[5px] right-0 top-0 absolute bg-zinc-300 rounded-[5px]" />
-            <div className="w-[30px] h-[5px] right-0 top-[11px] absolute bg-zinc-300 rounded-[5px]" />
-            <div className="w-[30px] h-[5px] right-0 top-[22px] absolute bg-zinc-300 rounded-[5px]" />
+          <div className={`w-[30px] h-[27px] top-[5px] right-0 relative`}>
+            <div className="w-[30px] h-[5px] right-[0px] top-0 absolute bg-zinc-300 rounded-[5px]" />
+            <div className="w-[30px] h-[5px] right-[0px] top-[11px] absolute bg-zinc-300 rounded-[5px]" />
+            <div className="w-[30px] h-[5px] right-[0px] top-[22px] absolute bg-zinc-300 rounded-[5px]" />
           </div>
         </button>
 
@@ -75,24 +75,39 @@ const Navbar = () => {
           className={`${renderMobileMenu ? "fade-in" : "hidden"}`}
         >
           <div className="absolute">
-            <div className="relative top-0 left-0">
-              <div className="w-[353px] h-[199px] relative">
-                <div className="w-[30px] h-[30px] left-[323px] top-0 absolute">
+            <div className="w-[364px] h-[209.14px] relative">
+              <div className="w-[30px] h-[30px] left-[334px] top-[10.14px] absolute">
+                <div className="w-[36.37px] h-[6.06px] left-0 top-[25.71px] absolute origin-top-left -rotate-45 bg-zinc-300 rounded-[3px]" />
+                <div className="w-[36.37px] h-[6.06px] left-[4.29px] top-0 absolute origin-top-left rotate-45 bg-zinc-300 rounded-[3px]" />
+              </div>
+              <div className="w-[364px] h-[47.23px] left-0 top-0 absolute">
+                <div className="w-[219px] h-[47.23px] left-0 top-0 absolute">
+                  <div className="w-[161px] h-[38px] left-[58px] top-[2.14px] absolute text-purple-700 text-[38px] font-normal font-['Inika']">
+                    QuikSeat
+                  </div>
+                  <div className="w-[47px] h-[47.23px] left-0 top-0 absolute">
+                    <div className="w-[10.91px] h-[10.91px] left-[-0px] top-[34.47px] absolute origin-top-left rotate-[-1.23deg] bg-indigo-900 rounded-full" />
+                    <div className="w-[10.91px] h-[10.91px] left-[35.86px] top-[34.58px] absolute origin-top-left rotate-[-1.23deg] bg-purple-700 rounded-full" />
+                    <div className="w-[33.13px] h-[11.52px] left-[4.91px] top-[28.40px] absolute origin-top-left rotate-[-59deg] bg-purple-700 rounded-[23px]" />
+                    <div className="w-[29.47px] h-[11.52px] left-[15.54px] top-[41.30px] absolute origin-top-left rotate-[-59deg] bg-indigo-900 rounded-[23px]" />
+                  </div>
+                </div>
+                <div className="w-[30px] h-[30px] left-[334px] top-[10.14px] absolute">
                   <div className="w-[36.37px] h-[6.06px] left-0 top-[25.71px] absolute origin-top-left -rotate-45 bg-zinc-300 rounded-[3px]" />
                   <div className="w-[36.37px] h-[6.06px] left-[4.29px] top-0 absolute origin-top-left rotate-45 bg-zinc-300 rounded-[3px]" />
                 </div>
-                <div className="w-[353px] h-[149px] left-0 top-[50px] absolute bg-neutral-800/opacity-95 rounded-[14px] border-2 border-neutral-700" />
-                <div className="w-[319px] h-0.5 left-[20px] top-[97px] absolute bg-neutral-700 rounded-sm" />
-                <div className="w-[319px] h-0.5 left-[19px] top-[144px] absolute bg-neutral-700 rounded-sm" />
-                <div className="left-[267px] top-[63px] absolute text-white text-[22px] font-normal font-['Inter']">
-                  Pricing
-                </div>
-                <div className="left-[256px] top-[153px] absolute text-white text-[22px] font-normal font-['Inter']">
-                  Contact
-                </div>
-                <div className="left-[226px] top-[108px] absolute text-white text-[22px] font-normal font-['Inter']">
-                  Dashboard
-                </div>
+              </div>
+              <div className="w-[353px] h-[149px] left-[11px] top-[60.14px] absolute bg-neutral-800/opacity-95 rounded-[14px] border-2 border-neutral-700" />
+              <div className="w-[319px] h-0.5 left-[31px] top-[107.14px] absolute bg-neutral-700 rounded-sm" />
+              <div className="w-[319px] h-0.5 left-[30px] top-[154.14px] absolute bg-neutral-700 rounded-sm" />
+              <div className="left-[278px] top-[73.14px] absolute text-white text-[22px] font-normal font-['Inter']">
+                Pricing
+              </div>
+              <div className="left-[267px] top-[163.14px] absolute text-white text-[22px] font-normal font-['Inter']">
+                Contact
+              </div>
+              <div className="left-[237px] top-[118.14px] absolute text-white text-[22px] font-normal font-['Inter']">
+                Dashboard
               </div>
             </div>
           </div>
