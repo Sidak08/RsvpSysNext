b01a7fad0e89d8aaac4d5407466e3e231be64ac0
Sidak <magicalsidak@gmail.com>
2024-08-01 00:13:15 -0400

    fin the ui for both the booking pages

--- src/app/book/[book]/info/[info]/page.jsx
+++ src/app/book/[book]/info/[info]/page.jsx
@@ -1,8 +1,115 @@
+"use client";
+import { useState, Suspense } from "react";
+import axios from "axios";
+import Link from "next/link";
+import Cookies from "js-cookie";
+import { useRouter, useSearchParams } from "next/navigation";
+import clsx from "clsx";
+
 export default function Page({ params }) {
   console.log(params);
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [error, setError] = useState({
+    success: true,
+    error: "",
+  });
+
+  const sendInfo = () => {
+    if (name.trim().split(" ").length === 2) {
+      axios
+        .post(`/api/auth/signup`, {
+          email: email.trim().toLowerCase(),
+          password: password,
+          id: Math.floor(Math.random() * 900000000) + 100000000,
+          firstName: name.trim().split(" ")[0],
+          lastName: name.trim().split(" ")[1],
+          phone: phone.trim(),
+          dashboardData: {
+            elementsArray: [],
+            linesArray: [
+              {
+                x: false,
+                y: false,
+              },
+            ],
+            upComingReservations: [],
+          },
+        })
+        .then((res) => {
+          if (res.data.success === true) {
+            Cookies.set("loginInfo", JSON.stringify({ email, password }), {
+              expires: 30,
+            });
+            router.push("/dashboard");
+          }
+          if (Object.keys(res.data.errors).length > 0) {
+            const errorStr = Object.keys(res.data.errors)
+              .map((key) => {
+                return `${res.data.errors[key]}`;
+              })
+              .join(" ");
+            setError({ success: res.data.success, errors: errorStr });
+          }
+        });
+    } else {
+      setError({
+        success: false,
+        errors: "Please Enter Your first and last name seprated by a space",
+      });
+    }
+  };
+
   return (
-    <div>
-      info: {params.info} {params.book}
+    <div className="h-[100vh] w-full flex justify-center items-center bg-[#0d0d0d]">
+      <div className="h-[650px] max-w-[510px] w-[90%] bg-[#1b1a1a] rounded-[15px] flex flex-col items-center justify-evenly">
+        <div className="h-16 w-[90%]">
+          <h1 className="text-4xl text-white">Your Info</h1>
+          <div className="w-full h-[2px] bg-white"></div>
+        </div>
+        <div className="w-[90%] h-[395px] flex flex-col justify-between items-end mb-[60px]">
+          <div className="h-[90px] w-full">
+            <h2 className="text-2xl mb-2">Full Name</h2>
+            <input
+              type="text"
+              className="h-[50px] w-full border border-[#464444] bg-[#212121] rounded-lg text-white pl-2"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              placeholder="Type In Your First And Last Name Ex. John Doe"
+            />
+          </div>
+          <div className="h-[90px] w-full mt-2">
+            <h2 className="text-2xl mb-2">Phone Number</h2>
+            <input
+              type="tel"
+              className="h-[50px] w-full border border-[#464444] bg-[#212121] rounded-lg text-white pl-2"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+              placeholder="Type In Your Phone Number"
+            />
+          </div>
+          <div className="h-[90px] w-full">
+            <h2 className="text-2xl mb-2">Email</h2>
+            <input
+              type="email"
+              className="h-[50px] w-full border border-[#464444] bg-[#212121] rounded-lg text-white pl-2"
+              value={email}
+              onChange={(e) => setEmail(e.target.value.toLowerCase().trim())}
+              placeholder="Type In Your Email Address"
+            />
+          </div>
+          <div className="h-[50px] w-full mt-2 bg-[#313131] border border-[#464444] rounded-lg"></div>
+        </div>
+        <button
+          className="h-[50px] w-[90%] bg-[#320db1] text-white text-lg rounded-lg mt-4 cursor-pointer "
+          onClick={sendInfo}
+        >
+          Book
+        </button>
+      </div>
     </div>
   );
 }
--- src/app/book/[book]/timeslot/page.jsx
+++ src/app/book/[book]/timeslot/page.jsx
@@ -14,9 +14,11 @@ export default function Page({ params }) {
 
   const [selectedTime, setSelectedTime] = useState("17:00 - 21:00");
 
+  const findTime = () => {};
+
   return (
     <div className="flex items-center justify-center w-full h-[100vh]">
-      <div className="w-[90%] max-w-[750px] h-[500px] bg-[#1b1a1a] rounded-[13px] flex justify-evenly items-center flex-col md-h-[738px]">
+      <div className="w-[90%] max-w-[750px] md:h-[500px] bg-[#1b1a1a] rounded-[13px] flex justify-evenly items-center flex-col h-[738px]">
         <div className="flex flex-col justify-evenly items-start w-[90%] ml-[1.5%]">
           <div className="text-white text-[46px] font-medium font-['Inter']">
             Book A Table
@@ -24,11 +26,13 @@ export default function Page({ params }) {
           <div className="w-full h-[3px] bg-[#d9d9d9] rounded-[13px]" />
         </div>
 
-        <div className="flex flex-col justify-evenly items-center w-full">
-          <div className="text-white text-2xl font-bold font-['Inika']">
-            Closet Time Slots
+        <div className="w-[95%] flex flex-col justify-evenly items-center bg-[#272626] rounded-[13px] py-4 ">
+          <div className="w-full pl-5">
+            <div className="text-white text-3xl font-bold font-['Inika'] mb-2">
+              Closet Time Slots
+            </div>
           </div>
-          <div className="flex md:flex-row flex-col justify-evenly items-center w-full">
+          <div className="flex md:flex-row flex-col justify-evenly items-center w-full px-2">
             <BookTime
               time={bookTimeSlot.firstTime}
               setBookTime={setSelectedTime}
@@ -43,11 +47,53 @@ export default function Page({ params }) {
             />
           </div>
         </div>
+
+        <div className="flex md:flex-row flex-col justify-evenly items-center w-[95%] px-2">
+          <div className="flex items-center justify-evenly flex-col h-[120px] md:w-[60%] w-full">
+            <div className="flex items-center justify-evenly flex-col w-full">
+              <div className="w-full">
+                <div className="text-white text-2xl font-bold font-['Inika'] mb-2">
+                  Choose Your Own Time
+                </div>
+              </div>
+              <div className="w-[90%] h-[7px] bg-[#d9d9d9]" />
+            </div>
+            <input className="w-[90%] h-[38px] bg-[#313131]" type="calender" />
+          </div>
+          <button
+            className="w-[90%] md:w-[40%] h-[50px] bg-[#3f12d7] rounded-[13px] flex items-center justify-center"
+            onClick={findTime}
+          >
+            <div className="w-[102px] h-[30px] text-white text-xl font-bold font-['Inika']">
+              Find Time
+            </div>
+          </button>
+        </div>
       </div>
     </div>
   );
 }
 
 const BookTime = ({ time, setBookTime }) => {
-  return <div className="w-[90%] h-[88px] bg-[#313131] rounded-[10px]"> </div>;
+  return (
+    <div className="w-[90%] h-[88px] bg-[#313131] rounded-[10px] m-2 flex flex-col item-center justify-between">
+      <div className="h-[50%] w-full flex items-center justify-center">
+        {" "}
+        <div className="text-white text-xl font-bold font-['Inika']">
+          {" "}
+          {time}{" "}
+        </div>
+      </div>
+      <button
+        className="h-[50%] w-full bg-[#3f12d7] rounded-[10px] flex items-center justify-center"
+        onClick={() => {
+          setBookTime(time);
+        }}
+      >
+        <div className="text-white text-xl font-bold font-['Inika'] text-center">
+          Book
+        </div>
+      </button>
+    </div>
+  );
 };
